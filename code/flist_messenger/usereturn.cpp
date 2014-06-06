e6a4e88f0ae18cf6b5791744a5cc974551661b3b
kythyria <kythyria@berigora.net>
2014-06-06 20:00:13 +0100

    Create a signal-using return filter.
    The existing two could be coalesced into one by using appropriately
    connected signals. So create a suitably signally one.

--- code/flist_messenger/usereturn.cpp
+++ code/flist_messenger/usereturn.cpp
@@ -39,3 +39,30 @@ bool UseReturn::eventFilter ( QObject* obj, QEvent* event )
 		return QObject::eventFilter ( obj, event );
 	}
 }
+
+ReturnFilter::ReturnFilter(QObject *parent) : QObject(parent) { }
+
+bool ReturnFilter::eventFilter(QObject *obj, QEvent *event)
+{
+	if(event->type() != QEvent::KeyPress) { return QObject::eventFilter(obj, event); }
+
+	QKeyEvent *keyEvent = static_cast<QKeyEvent *> ( event );
+	switch ( keyEvent->key() )
+	{
+	case Qt::Key_Enter:
+	case Qt::Key_Return:
+		emit enter(keyEvent->modifiers());
+
+		switch(keyEvent->modifiers())
+		{
+		case Qt::ControlModifier:	emit controlEnter(); break;
+		case Qt::ShiftModifier:   emit shiftEnter();   break;
+		default:                  emit plainEnter();   break;
+		}
+
+		return true;
+
+	default:
+		return QObject::eventFilter(obj, event);
+	}
+}
--- code/flist_messenger/usereturn.h
+++ code/flist_messenger/usereturn.h
@@ -19,4 +19,21 @@ protected:
 	bool eventFilter ( QObject *obj, QEvent *event );
 };
 
+class ReturnFilter : public QObject
+{
+	Q_OBJECT
+
+public:
+	ReturnFilter(QObject* parent);
+
+signals:
+	void controlEnter();
+	void shiftEnter();
+	void plainEnter();
+	void enter(Qt::KeyboardModifiers modifiers);
+
+protected:
+	bool eventFilter ( QObject *obj, QEvent *event );
+};
+
 #endif // USERETURN_H
